e0822992b2e1f815ee2febb976fb413765965e6b
Christian Vuerings <vueringschristian@gmail.com>
2024-11-07 06:38:28 -0800

    update

--- src/memwatch.cc
+++ src/memwatch.cc
@@ -160,6 +160,7 @@ NAN_GC_CALLBACK(memwatch::after_gc) {
             s_stats.gcProcessWeakCallbacksTime += gcTime;
             return;
 
+		case kGCTypeMinorMarkCompact:
         case kGCTypeMarkSweepCompact:
         case kGCTypeAll:
             break;
